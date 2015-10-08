34730e45c6af7c90359b86dc9d5945cdef3019e2
Acedrin <victor.enaud@hotmail.fr>
2015-10-08 08:46:06 +0200

    Début des requêtes

---
+++ TP_BDONN/src/tp_bdonn/FlotteCamion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp_bdonn;
+
+import java.sql.*;
+
+
+/**
+ *
+ * @author victo
+ */
+public class FlotteCamion {
+
+    public static void callFlotte(Connection con) {
+        try {
+            String query = "SELECT Camion.Immatriculation,Chauffeur.Chauffeur_Nom, Chauffeur.Chauffeur_Prenom"
+                    + "     FROM Camion NATURAL FULL OUTER JOIN Chauffeur WHERE ?";
+
+            PreparedStatement stmt = con.prepareStatement(query);
+
+            stmt.setString(1, "%");
+            ResultSet res = stmt.executeQuery();
+
+            while (res.next()) {
+
+                System.out.println("Info : " + res.getString("immatriculation") + " affilié à "
+                        + res.getString("chauffeur_nom") + " " + res.getString("chauffeur_prenom"));
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
+}
--- TP_BDONN/src/tp_bdonn/TP_BDONN.java
+++ TP_BDONN/src/tp_bdonn/TP_BDONN.java
@@ -22,35 +22,14 @@ public class TP_BDONN {
         try {
             Class.forName("org.postgresql.Driver");
 
-            // String dbURL = "jdbc:postgresql://localhost:5432/TP_BDONN";
-            // String user = "postgres";
-            // String password = "deadlands";
             String dbURL = "jdbc:postgresql://localhost:5432/TP_BDONN";
             String user = "postgres";
             String password = "BDONN01";
 
             Connection con = DriverManager.getConnection(dbURL, user, password);
-
-            String query = "SELECT * FROM entreprise WHERE entreprise_nom LIKE ?";
-            // String query = "SELECT * FROM entreprise WHERE entreprise_nom LIKE '%'";
-            //String query1 = "SELECT Camion.Immatriculation,Chauffeur.Chauffeur_Nom, Chauffeur.Chauffeur_Prenom"
-            //        + "     FROM Camion NATURAL FULL OUTER JOIN Chauffeur";
             
-            PreparedStatement stmt = con.prepareStatement(query);
-            //Statement stmt = con.createStatement();
-            
-            stmt.setString(1, "%");
+            FlotteCamion.callFlotte(con);
 
-            ResultSet res = stmt.executeQuery();
-            //ResultSet res = stmt.executeQuery(query1);
-
-            while (res.next()) {
-                  System.out.println("Info :" + res.getString("entreprise_nom"));
-            //    System.out.println("Info : " + res.getString("immatriculation") + " affilié à "
-            //            + res.getString("chauffeur_nom") + " " + res.getString("chauffeur_prenom"));
-            }
-            
-            stmt.close();
             con.close();
             
         } catch (ClassNotFoundException e) {
